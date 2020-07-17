1306826222258a42a753ad8bbc001d84cc0170cf
Lokeshthakasi <lokeshthakasi@gmail.com>
2020-07-17 15:42:50 +0530

    Get Correct sentence button sentences are generated

--- RDIP/Codes/tasks.js
+++ RDIP/Codes/tasks.js
@@ -1,9 +1,9 @@
-var correct_english_sentence=["John ate an apple before afternoon","some students like to study in the night","John and Mary went to church","John went to church after eating","did he go to market","the woman who called my sister sells cosmetics","John goes to the library and studies","the teacher returned the book after she noticed the error","I told her that I bought a book yesterday","John ate an apple so did she"]
+var correct_english_sentence = ["John ate an apple before afternoon","some students like to study in the night","John and Mary went to church","John went to church after eating","did he go to market","the woman who called my sister sells cosmetics","John goes to the library and studies","John ate an apple so did she","the teacher returned the book after she noticed the error","I told her that I bought a book yesterday"]
 
-var eng1=[["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"],
+var eng1 = [["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"],
 ["some students like to study in the night","at night some students like to study"],
 ["John and Mary went to church","Mary and John went to church"],
-["John went to church after eating",  "after eating John went to church","John after eating went to church"],
+["John went to church after eating","after eating John went to church","John after eating went to church"],
 ["did he go to market","he did go to market"],
 ["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"],
 ["John goes to the library and studies","John studies and goes to the library"],
@@ -12,15 +12,16 @@ var eng1=[["John ate an apple before afternoon","before afternoon John ate an ap
 ["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her"  ,"yesterday I bought a book that I told her"]]
 
 
-var correct_hindi_sentence=["राम और श्याम बाजार गयें","राम सोया और श्याम भी","मैंने उसे बताया कि राम सो रहा है","राम खाकर सोया","बिल्लियों को मारकर कुत्ता सो गया"  ,"एक लाल किताब वहाँ है","एक बड़ी सी किताब वहाँ है  "]
+var correct_hindi_sentence = ["राम और श्याम बाजार गयें","राम सोया और श्याम भी","मैंने उसे बताया कि राम सो रहा है","राम खाकर सोया","बिल्लियों को मारकर कुत्ता सो गया" ,"एक लाल किताब वहाँ है","एक बड़ी सी किताब वहाँ है  "]
 
-var hin1=[["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"],
+
+var hin1 = [["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"],
 ["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"],
 ["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"],
 ["राम खाकर सोया","खाकर राम सोया","राम सोया खाकर","खाकर सोया राम","सोया राम खाकर","सोया खाकर राम"],
-["बिल्लियों को मारकर कुत्ता सो गया" ,"मारकर बिल्लियों को कुत्ता सो गया"  ,"बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता" ,"कुत्ता सो गया बिल्लियों को मारकर" ,"कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],
+["बिल्लियों को मारकर कुत्ता सो गया" ,"मारकर बिल्लियों को कुत्ता सो गया","बिल्लियों को मारकर सो गया कुत्ता","मारकर बिल्लियों को सो गया कुत्ता","कुत्ता सो गया बिल्लियों को मारकर","कुत्ता सो गया मारकर बिल्लियों को","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],
 ["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"],
-["एक बड़ी सी किताब वहाँ है ","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब"," है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]]
+["एक बड़ी सी किताब वहाँ है","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब","है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]];
 
 function lang1()
 {
@@ -32,6 +33,8 @@ a1=a.split(" ");
   document.getElementById("example5").innerHTML=""
   document.getElementById("example4").innerHTML=""
   document.getElementById("example6").innerHTML=""
+  document.getElementById("example7").innerHTML=""
+  document.getElementById("but5").innerHTML="Get Correct Sentence"
   document.getElementById("but3").style.visibility="hidden";
   document.getElementById('but4').style.visibility="hidden"
   document.getElementById('but5').style.visibility="hidden";
@@ -149,6 +152,7 @@ function reset()
       document.getElementById('example4').innerHTML=""
       document.getElementById('example5').innerHTML=""
       document.getElementById('example6').innerHTML=""
+      document.getElementById('example7').innerHTML=""
       document.getElementById('but3').style.visibility="hidden"
       document.getElementById('but4').style.visibility="hidden" 
       document.getElementById('but5').style.visibility="hidden"; 
@@ -220,5 +224,73 @@ else
   
 }
 
+}
+}
+
+function change()
+{
+d=correct_english_sentence.indexOf(a)
+u=eng1[d]
+e=correct_hindi_sentence.indexOf(b)
+w=hin1[e]
+
+
+
+if(document.getElementById("english").selected)
+  {
+    if(document.getElementById("but5").innerHTML=="Get Correct Sentence")
+    {
+       for(i=0;i<u.length;i++)
+  { 
+   document.getElementById("example7").innerHTML+=u[i]+"<br>";
+   document.getElementById("but5").innerHTML="Hide the correct Sentence" 
+} 
+}
+else if(document.getElementById("but5").innerHTML=="Hide the correct Sentence")
+  {
+    
+   document.getElementById("example7").innerHTML=""
+   document.getElementById("but5").innerHTML="Get Answers"
+}
+else if(document.getElementById("but5").innerHTML=="Get Answers")
+{
+
+       for(i=0;i<u.length;i++)
+  { 
+   document.getElementById("example7").innerHTML+=u[i]+"<br>";
+   document.getElementById("but5").innerHTML="Hide the correct Sentence" 
+} 
+}
+}
+
+else if(document.getElementById("hindi").selected)
+  { 
+
+    if(document.getElementById("but5").innerHTML=="Get Correct Sentence")
+    {
+       for(i=0;i<w.length;i++)
+  { 
+   document.getElementById("example7").innerHTML+=w[i]+"<br>";
+   document.getElementById("but5").innerHTML="Hide the correct Sentence" 
+} 
+}
+else if(document.getElementById("but5").innerHTML=="Hide the correct Sentence")
+  {
+    
+   document.getElementById("example7").innerHTML=""
+   document.getElementById("but5").innerHTML="Get Answers"
+}
+else if(document.getElementById("but5").innerHTML=="Get Answers")
+{
+
+  for(i=0;i<w.length;i++)
+
+  {
+   
+   document.getElementById("example7").innerHTML+=w[i]+"<br>";
+   document.getElementById("but5").innerHTML="Hide the correct Sentence" 
+} 
+ 
+}
 }
 }
